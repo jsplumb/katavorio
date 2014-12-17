017abb88099bc6f7e978c7ba8e7212211a7b6796
sporritt <simon.porritt@gmail.com>
2014-12-17 11:44:16 +1100

    udpate args passed from stop callback to support multiple element drag properly

--- src/katavorio-0.5.js
+++ src/katavorio-0.5.js
@@ -309,8 +309,25 @@
         };
 
         this.stop = function(e) {
-            if (moving)
-                this.params.events["stop"]({el:dragEl, pos:this.params.getPosition(dragEl), e:e, drag:this});
+            if (moving) {
+                var positions = [],
+                    sel = k.getSelection(),
+                    dPos = this.params.getPosition(dragEl);
+
+                if (sel.length > 1) {
+                    for (var i = 0; i < sel.length; i++) {
+                        var p = this.params.getPosition(sel[i].el);
+                        positions.push([ sel[i].el, { left: p[0], top: p[1] }, sel[i] ]);
+                    }
+                }
+                else {
+                    positions.push([ dragEl, {left:dPos[0], top:dPos[1]}, this ]);
+                }
+
+                this.params.events["stop"]({
+                    el: dragEl, pos: dPos, e: e, drag: this, selection:positions
+                });
+            }
         };
 
         this.mark = function() {
